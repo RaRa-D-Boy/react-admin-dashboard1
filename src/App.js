71faf48a5d267b9f4748c4cff24ebd6481f3d05a
RaRa-D-Boy <you@example.com>
2021-10-16 06:03:05 -0400

    Initial Commit Project created

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  
+        <p>
+         we've arrived
+        </p>
+     
+    </div>
+  );
+}
+
+export default App;
